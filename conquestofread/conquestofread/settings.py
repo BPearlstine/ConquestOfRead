c2399ebf480fdbd14c8230ad0d842c7c7c5d0588
Ben Pearlstine <ben.pearlstine@gmail.com>
2019-06-04 14:07:08 -0400

    updated app name

--- conquestofread/conquestofread/settings.py
+++ conquestofread/conquestofread/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'conquestofread.apps.blog'
+    'conquestofread.apps.podcast'
 ]
 
 MIDDLEWARE = [
