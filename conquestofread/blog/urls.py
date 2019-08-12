1bf5b36b9a80363fe8fc21c9b533044be4e56969
Ben Pearlstine <ben.pearlstine@gmail.com>
2019-08-12 09:32:00 -0400

    listviews not working

--- conquestofread/blog/urls.py
+++ conquestofread/blog/urls.py
@@ -4,5 +4,9 @@ from . import views
 urlpatterns = [
     path('new-post',
          views.AddBlogView.as_view(),
-         name='add_blog')
+         name='add_blog'),
+
+    path('blogs-by-tag/<int:pk>/',
+         views.BlogsByTagView.as_view(),
+         name='blogs_by_tag')
 ]
